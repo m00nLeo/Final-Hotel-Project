59803d16432d95749b75f94230a24918185c8cb7
Ngọc Trinh Phạm <trinhpn96@gmail.com>
2023-01-05 17:08:22 +0700

    feat: services

--- Hotel/tailwind.config.cjs
+++ Hotel/tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        h2: ["Raleway", ...defaultTheme.fontFamily.sans],
+        subHeading: ["Jost", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#343a40",
+        light: "#987648",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
