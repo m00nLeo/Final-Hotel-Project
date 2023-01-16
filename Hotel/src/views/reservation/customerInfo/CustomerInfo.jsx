8fbe1b67495d58c4f2b7d100c543f528714e6333
m00nLeo <tranlehoanglong2001@gmail.com>
2023-01-16 16:53:01 +0700

    feat. Create Reservation 2 & 3

---
+++ Hotel/src/views/reservation/customerInfo/CustomerInfo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SubHero from "../../contact/SubHero";
+
+const CustomerInfo = () => {
+  return (
+    <div>
+      {/* Hero */}
+      <SubHero title="Reservation" path="Reservation" />
+
+      {/* Content */}
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default CustomerInfo;
