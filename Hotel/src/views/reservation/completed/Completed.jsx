ee3e3fd505ac6c7d36f3e7a09f1fcf1576781bbb
diemnguyen244 <nguyentranhoaidiem@gmail.com>
2023-01-19 21:19:57 +0700

    update

--- Hotel/src/views/reservation/completed/Completed.jsx
+++ Hotel/src/views/reservation/completed/Completed.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import SubHero from "../../contact/SubHero";
-
+import { FaHeart, FaMale, FaRegHeart } from "react-icons/fa";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { GoCalendar } from "react-icons/go";
 const Completed = () => {
   return (
     <div>
@@ -8,7 +10,198 @@ const Completed = () => {
       <SubHero title="Reservation" path="Reservation" />
 
       {/* Content */}
-      <div className=""></div>
+      <div className=" md:mx-20 font-serif p-14 mt-10">
+        <div className=" lg:grid lg:grid-cols-12 md:grid-cols-12">
+          <div className=" mx-auto w-full lg:col-span-8 lg:col-start-3 md:col-span-10">
+            {/* thẻ customer infor */}
+            <div className="  border-solid border border-gray-200  px-8 bg-[#f8f9fA]">
+              <h3 className="py-5 text-2xl font-medium tracking-wide">
+                Rooms & Services
+              </h3>
+            </div>
+            {/* input */}
+            <div className="px-8 border-solid border border-gray-200 pt-5 font-sans">
+              <div className="border-b-2 mb-3 pb-3">
+                <div>
+                  <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-2 font-serif text-sm md:mb-2">
+                    {/* check in */}
+                    <div className="flex gap-1 mb-1">
+                      <GoCalendar className="w-5 h-5" />
+                      <span className="text-[#495057] font-thin ">
+                        Check-In Date:
+                      </span>
+                      <strong className="font-sans ">19 Feb 2023</strong>
+                    </div>
+                    {/* check out */}
+                    <div className="flex gap-1 mb-1">
+                      <GoCalendar className="w-5 h-5" />
+                      Check-Out Date:
+                      <strong className="font-sans ">20 Feb 2023</strong>
+                    </div>
+                    <div>
+                      <p className="font-sans mb-1">
+                        (Duration of stay:{" "}
+                        <strong className="font-sans">1 Night</strong> )
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex gap-1">
+                  <div>{<FaMale className="w-5 h-5" />}</div>
+                  <div className="font-sans ">
+                    <span>
+                      Adults: <strong>1</strong>
+                    </span>
+                    <span>-</span>
+                    <span>
+                      Children: <strong>0</strong>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
+              <div className="border-b-2  mb-3 pb-3">
+                <div className="mb-2">
+                  <span className=" text-lg font-medium font-serif ">
+                    Executive Deluxe Double or Twin Room
+                  </span>
+                  <div className="flex justify-between text-sm mb-1">
+                    <span className="text-[#495057] font-light  ">
+                      Quantity:{" "}
+                      <strong className="font-semibold">1 Room</strong>
+                    </span>
+                    <span className="text-[#b18c57]">
+                      <small>US$</small> 69.00
+                    </span>
+                  </div>
+                  <div className="flex justify-between text-sm ">
+                    <span className="text-[#495057] font-light  ">
+                      Extra beds:
+                      <strong className="font-semibold">1 Bed</strong>
+                    </span>
+                    <span className="text-[#b18c57]">
+                      <small>US$</small> 10.00
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <span className=" text-lg font-medium font-serif">
+                    Imperial Suite with Balcony City View
+                  </span>
+                  <div className="flex justify-between text-sm ">
+                    <span className="text-[#495057] font-light  ">
+                      Quantity:
+                      <strong className="font-semibold">1 Room</strong>
+                    </span>
+                    <span className="text-[#b18c57] ">
+                      <small>US$</small> 69.00
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="border-b-2  mb-3 pb-3">
+                <div className=" flex text-lg font-medium font-serif  justify-between">
+                  <span>Airport Transfer Service</span>
+                  <span className="text-[#b18c57] text-lg">
+                    <small>US$</small> 69.00
+                  </span>
+                </div>
+              </div>
+              <div className="border-b-2  mb-3 pb-3">
+                <div className=" flex text-lg font-medium font-serif justify-between">
+                  <span>Tax & Service Change</span>
+                  <span className="text-[#b18c57] text-lg">
+                    <small>US$</small> 14.80
+                  </span>
+                </div>
+              </div>
+
+              <div className=" flex text-lg font-medium font-serif justify-between">
+                <span>Total Amount:</span>
+                <span className="text-[#b18c57] text-xl">
+                  <small>US$</small> 162.80
+                </span>
+              </div>
+            </div>
+
+            <div className="mt-5">
+              <div className="  border-solid border border-gray-200  px-8 bg-[#f8f9fA]">
+                <h3 className="py-5 text-2xl tracking-wide ">
+                  Customer Information
+                </h3>
+              </div>
+              <div
+                className="px-8 border-solid border border-gray-200
+              pt-5 font-serif mb-5 text-xs"
+              >
+                <p className="mb-2 text-base">
+                  Full Name: <strong>Mr. Jonh Deep</strong>
+                </p>
+                <p className="mb-2 text-base">
+                  Email: <strong>booking@example.com</strong>
+                </p>
+                <p className="mb-2 text-base">
+                  Phone: <strong>+33 (0) 1 89 78 67 56</strong>
+                </p>
+                <p className="mb-2 text-base">
+                  Address:{" "}
+                  <strong>
+                    No 2201 rue des Petites Ecuries, 75010 Paris, France
+                  </strong>
+                </p>
+                <p className="mb-2 text-base">
+                  Country: <strong>France</strong>
+                </p>
+                <p className="mb-2 text-base">
+                  Your Requests:{" "}
+                  <strong>
+                    Lorem ipsum dolor sit amet, nam lorem etiam ad, ex nusquam
+                    luptatum vel, te eum numquam luptatum. Id sint ullum
+                    offendit eam, duo iisque quaerendum deterruisset ne.
+                  </strong>
+                </p>
+              </div>
+            </div>
+
+            {/*  */}
+            <div className="mt-5">
+              <div className="  border-solid border border-gray-200  px-8 bg-[#f8f9fA]">
+                <h3 className="py-5 text-xl font-light tracking-wide">
+                  Payment Method
+                </h3>
+              </div>
+              <div
+                className="px-8 border-solid border border-gray-200
+              pt-5 font-serif mb-8"
+              >
+                <div class="flex items-center mb-4">
+                  <p> Pay With Cash</p>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div>
+                  <a
+                    class="flex gap-2 items-center justify-center bg-[#aab0bc] text-white w-40 h-10 text-base"
+                    href="/reservation"
+                  >
+                    <AiOutlineArrowLeft />
+                    Back Step
+                  </a>
+                </div>
+                <div>
+                  <a
+                    class="flex gap-2 items-center justify-center bg-[#b18c57] text-white w-40 h-10 text-base "
+                    href="/reservation/information/completed"
+                  >
+                    Confirm & Finish
+                    <AiOutlineArrowRight />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- Hotel/src/views/reservation/customerInfo/CustomerInfo.jsx
+++ Hotel/src/views/reservation/customerInfo/CustomerInfo.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import SubHero from "../../contact/SubHero";
+
+const handleReservation = (e) => {
+  e.preventDefault();
+  const form = document.getElementById("reservationForm");
+  const formElements = Array.from(form.elements);
+  let valid = true;
+  formElements.forEach((element) => {
+    let val = element.value.trim();
+    if (val == "") {
+      element.classList.add("border-red-500");
+      element.classList.remove("border-gray-300");
+      valid = false;
+    } else {
+      element.classList.remove("border-red-500");
+      element.classList.add("border-gray-300");
+    }
+  });
+  // Form is valid
+  if (valid) {
+    window.location.replace("/reservation/complete");
+  }
+};
 
 const CustomerInfo = () => {
   return (
@@ -9,16 +32,18 @@ const CustomerInfo = () => {
 
       {/* Content */}
       <div className=" md:mx-20 font-serif p-14 mt-10">
-        <div className=" grid grid-cols-12">
-          <div className=" mx-auto w-full col-span-8 col-start-3 ">
-            {/* thẻ customer infor */}
-            <div className="  border-solid border border-gray-200  px-8 bg-[#f8f9fA]">
-              <h3 className="py-5 text-xl font-light ">Customer Information</h3>
-            </div>
-            {/* input */}
-            <div className="px-8 border-solid border border-gray-200 pt-5 font-sans">
-              <div>
-                <form>
+        <form id="reservationForm" onSubmit={handleReservation}>
+          <div className=" grid grid-cols-12">
+            <div className=" mx-auto w-full col-span-8 col-start-3 ">
+              {/* thẻ customer infor */}
+              <div className="  border-solid border border-gray-200  px-8 bg-[#f8f9fA]">
+                <h3 className="py-5 text-xl font-light ">
+                  Customer Information
+                </h3>
+              </div>
+              {/* input */}
+              <div className="px-8 border-solid border border-gray-200 pt-5 font-sans">
+                <div>
                   <div class="md:grid md:gap-6 mb-6 md:grid-cols-2">
                     {/* title */}
                     <div className="mb-5">
@@ -29,7 +54,9 @@ const CustomerInfo = () => {
                         Title <span className="text-red-400 ">*</span>
                       </label>
                       <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option selected>Select</option>
+                        <option selected value="">
+                          Select
+                        </option>
                         <option value="Mr">Mr</option>
                         <option value="Mss">Mss</option>
                         <option value="Mrs">Mrs</option>
@@ -49,7 +76,6 @@ const CustomerInfo = () => {
                         type="text"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder=""
-                        required
                       />
                     </div>
 
@@ -66,7 +92,6 @@ const CustomerInfo = () => {
                         id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder=""
-                        required
                       />
                     </div>
                     {/*  */}
@@ -82,7 +107,6 @@ const CustomerInfo = () => {
                         id="phone"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder=""
-                        required
                       />
                     </div>
                     {/*  */}
@@ -98,7 +122,6 @@ const CustomerInfo = () => {
                         id="address"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder=""
-                        required
                       />
                     </div>
                     {/*  */}
@@ -108,7 +131,7 @@ const CustomerInfo = () => {
                         <span className="text-red-400 font-light">*</span>
                       </label>
                       <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option selected className="text-2xl">
+                        <option selected className="text-2xl" value="">
                           Select
                         </option>
                         <option value="AG">Afganistan</option>
@@ -136,102 +159,74 @@ const CustomerInfo = () => {
                       id="request"
                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-52 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder=""
-                      required
                     />
                   </div>
-                </form>
+                </div>
               </div>
-            </div>
-            <div className="mt-5">
-              <div className="  border-solid border border-gray-200  px-8 bg-[#f8f9fA]">
-                <h3 className="py-5 text-xl font-light ">Payment Method</h3>
-              </div>
-              <div
-                className="px-8 border-solid border border-gray-200
+              <div className="mt-5">
+                <div className="  border-solid border border-gray-200  px-8 bg-[#f8f9fA]">
+                  <h3 className="py-5 text-xl font-light ">Payment Method</h3>
+                </div>
+                <div
+                  className="px-8 border-solid border border-gray-200
               pt-5 font-serif mb-5"
-              >
-                <div class="flex items-center mb-4">
-                  <input
-                    id="default-radio-1"
-                    type="radio"
-                    value=""
-                    name="default-radio"
-                    class="w-4 h-4 text-[#b18c57]  bg-[#b18c57] "
-                  />
-                  <label
-                    for="default-radio-1"
-                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Pay With Cash
-                  </label>
-                </div>
-                <div class="flex items-center mb-5">
-                  <input
-                    checked
-                    id="default-radio-2"
-                    type="radio"
-                    value=""
-                    name="default-radio"
-                    class="w-4 h-4 bg-[#b18c57]  "
-                  />
-                  <label
-                    for="default-radio-2"
-                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Pay Online With Visa Card, Master Card
-                  </label>
-                </div>
-              </div>
-              <div className="flex justify-between">
-                <div>
-                  <a
-                    class="flex gap-2 items-center justify-center bg-[#aab0bc] text-white w-40 h-10 text-base"
-                    href="/reservation"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 18 18"
-                      strokeWidth={1}
-                      stroke="currentColor"
-                      height="0.6em"
-                      width="0.6em"
+                >
+                  <div class="flex items-center mb-4">
+                    <input
+                      id="default-radio-1"
+                      type="radio"
+                      value="cash"
+                      name="default-radio"
+                      class="w-4 h-4 text-[#b18c57]  bg-[#b18c57] "
+                    />
+                    <label
+                      for="default-radio-1"
+                      class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
-                      />
-                    </svg>
-                    Back Step
-                  </a>
-                </div>
-                <div>
-                  <a
-                    class="flex gap-2 items-center justify-center bg-[#b18c57] text-white w-40 h-10 text-base"
-                    href="/reservation/information/completed"
-                  >
-                    Next Step{" "}
-                    <svg
-                      stroke="currentColor"
-                      fill="currentColor"
-                      stroke-width="0"
-                      viewBox="0 0 16 16"
-                      height="1em"
-                      width="1em"
-                      xmlns="http://www.w3.org/2000/svg"
+                      Pay With Cash
+                    </label>
+                  </div>
+                  <div class="flex items-center mb-5">
+                    <input
+                      checked
+                      id="default-radio-2"
+                      type="radio"
+                      value="online"
+                      name="default-radio"
+                      class="w-4 h-4 bg-[#b18c57]  "
+                    />
+                    <label
+                      for="default-radio-2"
+                      class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                     >
-                      <path
-                        fill-rule="evenodd"
-                        d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
-                      ></path>
-                    </svg>
-                  </a>
+                      Pay Online With Visa Card, Master Card
+                    </label>
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <div>
+                    <a
+                      class="flex gap-2 items-center justify-center bg-[#aab0bc] text-white w-40 h-10 text-base"
+                      href="/reservation"
+                    >
+                      <AiOutlineArrowLeft />
+                      Back Step
+                    </a>
+                  </div>
+                  <div>
+                    <button
+                      value="next"
+                      type="submit"
+                      class="flex gap-2 items-center justify-center bg-[#b18c57] text-white w-40 h-10 text-base"
+                    >
+                      Next Step <AiOutlineArrowRight />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
